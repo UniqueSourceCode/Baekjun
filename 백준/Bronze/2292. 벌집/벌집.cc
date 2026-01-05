4820c566d03cdf37f61ea1d07094d8ea7efffbbf
SomeRandomKorean <104520417+SomeRandomKorean@users.noreply.github.com>
2026-01-05 13:27:12 +0900

    [Bronze II] Title: 벌집, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int room = 1;
+    int upper = 1;
+    while (true) {
+        if (n <= upper) break;
+        upper += 6*room++;
+    }
+    cout << room;
+    return 0;
+}
